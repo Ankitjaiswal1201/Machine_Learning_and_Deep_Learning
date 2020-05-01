94e342fc0404b41619788e673987cb8e2f9e3969
Ankit Jaiswal <ankitjaiswal1201@gmail.com>
2020-05-01 23:06:42 +0200

    Add files via upload

--- Handwritten_Digit_Recog/mnist_data.py
+++ Handwritten_Digit_Recog/mnist_data.py
@@ -11,6 +11,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 import numpy as np
+import matplotlib.pyplot as plt
 
 batch_size = 128
 num_classes = 10
@@ -21,6 +22,18 @@ img_rows, img_cols = 28, 28
 
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# plot 4 images as gray scale
+plt.subplot(221)
+plt.imshow(x_train[0], cmap=plt.get_cmap('gray'))
+plt.subplot(222)
+plt.imshow(x_train[1], cmap=plt.get_cmap('gray'))
+plt.subplot(223)
+plt.imshow(x_train[2], cmap=plt.get_cmap('gray'))
+plt.subplot(224)
+plt.imshow(x_train[3], cmap=plt.get_cmap('gray'))
+# show the plot
+plt.show()
 
 x_train = x_train.reshape(60000,28,28,1) # 60000 training images
 x_test = x_test.reshape(10000,28,28,1) # 10000 test images
